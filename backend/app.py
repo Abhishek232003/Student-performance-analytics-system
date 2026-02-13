e027377701d4cb15b0263771ca7c3513679c63e8
Abhishek <abhititu2003@gmail.com>
2026-02-13 11:04:20 +0530

    Added request routes and integrated Requests table with Aiven DB

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 
 from routes.auth_routes import auth_bp
 from routes.student_routes import student_bp
+from routes.request_routes import request_bp
 
 load_dotenv()
 
@@ -13,6 +14,7 @@ CORS(app)
 # Register routes
 app.register_blueprint(auth_bp, url_prefix="/api/auth")
 app.register_blueprint(student_bp, url_prefix="/api/students")
+app.register_blueprint(request_bp, url_prefix="/api/requests")
 
 
 @app.route("/")
